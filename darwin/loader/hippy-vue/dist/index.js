b1e8c835a0f836563ab1796e931fa8612881ad6b
humbass@gmail.com <job@meitehudong.com>
2020-09-07 11:11:19 +0800

    1

--- darwin/loader/hippy-vue/dist/index.js
+++ darwin/loader/hippy-vue/dist/index.js
@@ -1,7 +1,7 @@
 /*!
  * @hippy/vue v2.0.3
  * (Using Vue v2.6.11)
- * Build at: Mon Sep 07 2020 10:31:52 GMT+0800 (China Standard Time)
+ * Build at: Mon Sep 07 2020 11:10:12 GMT+0800 (China Standard Time)
  *
  * Tencent is pleased to support the open source community by making
  * Hippy available.
