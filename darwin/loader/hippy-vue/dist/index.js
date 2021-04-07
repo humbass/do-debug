c91c3fd855cde85a8b3c7c23bc2cff642a4bc1b0
humbass@gmail.com <job@meitehudong.com>
2021-04-07 17:40:48 +0800

    1

--- darwin/loader/hippy-vue/dist/index.js
+++ darwin/loader/hippy-vue/dist/index.js
@@ -1,7 +1,7 @@
 /*!
  * @hippy/vue v2.1.4
  * (Using Vue v2.6.11)
- * Build at: Tue Apr 06 2021 17:49:52 GMT+0800 (China Standard Time)
+ * Build at: Wed Apr 07 2021 17:06:16 GMT+0800 (China Standard Time)
  *
  * Tencent is pleased to support the open source community by making
  * Hippy available.
@@ -12718,11 +12718,6 @@ var ul = {
         case 'onScroll':
           event.offsetX = nativeEventParams.contentOffset.x;
           event.offsetY = nativeEventParams.contentOffset.y;
-          break;
-        case 'onLoadMore':
-        case 'endReached':
-          event.nativeEventName = 'endReached';
-          event.nativeEventParams = nativeEventParams;
           break;
       }
       return event;
